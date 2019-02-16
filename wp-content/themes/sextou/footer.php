2fe9513f8ad104e8be24227e18f0a9c0e46a46aa
Dico Didiraja <didiraja@hotmail.com>
2019-02-16 15:21:49 -0200

    otimizadas dependencias da página

--- wp-content/themes/sextou/footer.php
+++ wp-content/themes/sextou/footer.php
@@ -12,7 +12,7 @@
     </div>
 </footer>
 
-<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+<script src="<?= get_template_directory_uri(); ?>/assets/js/plugin/jquery-3.3.1.min.js"></script>
 <script src="<?= get_template_directory_uri(); ?>/assets/js/plugin/slick/slick.min.js"></script>
 <script src="<?= get_template_directory_uri(); ?>/assets/js/main.js"></script>
 
--- wp-content/themes/sextou/header.php
+++ wp-content/themes/sextou/header.php
@@ -7,17 +7,12 @@
     <meta name="description" content="">
     <meta name="author" content="">
     
-    <link rel="icon" href="../../../../favicon.ico">
+    <link rel="icon" href="<?= get_template_directory_uri(); ?>/assets/img/favicon.png">
 
     <title>Sextou!</title>
 
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,600i,700,700i,800" rel="stylesheet">
-
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
-
     <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    
+    <link rel="stylesheet" href="<?= get_template_directory_uri(); ?>/assets/css/bootstrap.min.css" >
 
     <link rel="stylesheet" type="text/css" href="<?= get_template_directory_uri(); ?>/assets/js/plugin/slick/slick.css"/>
     <link rel="stylesheet" href="<?= get_template_directory_uri(); ?>/assets/js/plugin/slick/slick-theme.css">
