71fc1dc29c57e627d69ca2c11dcc9822a0241c0e
Reinaldo Amorim <Juuhz@users.noreply.github.com>
2019-02-26 18:17:34 -0300

    add function pagination

--- wp-content/themes/sextou/functions.php
+++ wp-content/themes/sextou/functions.php
@@ -192,4 +192,86 @@ function mytheme_infinite_scroll_render() {
   get_template_part( ‘loop’ );
 }
 
+// Post Navi
+	function get_archive_nav() {
+
+			$args = array(
+				'post_type' 		=> 'post',
+				'posts_per_page'	=> -1
+			);
+
+			$posts = new WP_Query( $args );
+			$posts->max_num_pages = ceil( ( $posts->found_posts / 10 ) );
+
+		
+		if( $posts->max_num_pages <= 1 )
+			return;
+
+		$paged = get_query_var( 'paged' ) ? absint( get_query_var( 'paged' ) ) : 1;
+		$max   = ceil( $posts->max_num_pages );
+
+
+		if ( $paged >= 1 )
+			$links[] = $paged;
+
+
+		if ( $paged >= 3 ) {
+			$links[] = $paged - 1;
+			$links[] = $paged - 2;
+		}
+
+		if ( ( $paged + 2 ) <= $max ) {
+			$links[] = $paged + 2;
+			$links[] = $paged + 1;
+		}
+
+		$pathUrl = $_SERVER['REQUEST_URI'];
+		if( strpos( $pathUrl, '/page/' ) === false ){
+			$fullUrl = "http://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}page/";
+		}else{
+			$fullUrl = "http://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
+			$fullUrl = explode( '/page/', $fullUrl );
+			$fullUrl = $fullUrl[0] . '/page/';
+		}
+
+		echo '<div class="navigation"><ul>' . "\n";
+
+
+		if ( $paged >= 2 )
+			echo '<li class="first"><a href="'. $fullUrl . ( $paged - 1 ) .'"><</a></li>';
+
+		/**	Link to first page, plus ellipses if necessary */
+		if ( ! in_array( 1, $links ) ) {
+			$class = 1 == $paged ? ' class="active"' : '';
+
+			printf( '<li%s><a href="%s">%s</a></li>' . "\n", $class, esc_url( get_pagenum_link( 1 ) ), '1' );
+
+			if ( ! in_array( 2, $links ) )
+				echo '<li class="nope first"><a href="javascript:void(0);">…</a></li>';
+		}
+
+		/**	Link to current page, plus 2 pages in either direction if necessary */
+		sort( $links );
+		foreach ( (array) $links as $link ) {
+			$class = $paged == $link ? ' class="active"' : '';
+			printf( '<li%s><a href="%s">%s</a></li>' . "\n", $class, esc_url( get_pagenum_link( $link ) ), $link );
+		}
+
+		/**	Link to last page, plus ellipses if necessary */
+		if ( ! in_array( $max, $links ) ) {
+			if ( ! in_array( $max - 1, $links ) )
+				echo '<li class="nope last"><a href="javascript:void(0);">…</a></li>' . "\n";
+
+			$class = $paged == $max ? ' class="active"' : '';
+			printf( '<li%s><a href="%s">%s</a></li>' . "\n", $class, esc_url( get_pagenum_link( $max ) ), $max );
+		}
+
+		/**	Next Post Link */
+		if ( $paged >= 1 && $paged < $max )
+			echo '<li class="next"><a href="'. $fullUrl . ( $paged + 1 ) .'">></a></li>';
+
+		echo '</ul></div>' . "\n";
+
+	}
+
 ?>
